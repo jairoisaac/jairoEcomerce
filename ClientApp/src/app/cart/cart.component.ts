03a66a90a15bbc2cf890f034908fa1fd93068e80
Jairo Millan <54375045+jairoisaac@users.noreply.github.com>
2020-12-18 23:24:18 -0500

    Added delete, increase and decrease of items to the order

--- ClientApp/src/app/cart/cart.component.ts
+++ ClientApp/src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { findIndex } from 'rxjs/operators';
 import { DataService } from '../shared/data.service';
 
 
@@ -13,8 +14,24 @@ export class CartComponent implements OnInit {
 
   ngOnInit() {
   }
-  public itemToDelete(item) {
-  
-    alert("This is the number " + item.productId);
+  public itemToDecrese(item) {
+    if (item.quantity > 1) {
+      item.quantity = item.quantity - 1
+      //alert("Id " + item.productId + " Quantity " + item.quantity);
+    } else if (item.quantity === 1)
+    {
+
+      // delete that index
+      var myIndex = this.data.order.items.findIndex(i => i.productId == item.productId);
+      this.data.order.items.splice(myIndex, 1);
+      //alert(" Index " + myIndex);
+    }
   }
-}
+  public itemToIncrease(item) {
+    item.quantity = item.quantity + 1;
+  }
+  public ItemToDelete(item) {
+    var myIndex = this.data.order.items.findIndex(i => i.productId == item.productId);
+    this.data.order.items.splice(myIndex, 1);
+  }
+} 
