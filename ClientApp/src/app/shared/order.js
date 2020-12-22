775718f8823883c7c2ab2bfadcfb67ac7e2cb44e
Jairo Millan <54375045+jairoisaac@users.noreply.github.com>
2020-12-22 16:04:58 -0500

    addition of the cart

--- ClientApp/src/app/shared/order.js
+++ ClientApp/src/app/shared/order.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.OrderItem = exports.Order = void 0;
 var _ = require("lodash");
+//NOTE: Put the count here make a property for this class
 var Order = /** @class */ (function () {
     function Order() {
         this.items = new Array();
@@ -9,6 +10,13 @@ var Order = /** @class */ (function () {
     Object.defineProperty(Order.prototype, "subtotal", {
         get: function () {
             return _.sum(_.map(this.items, function (i) { return i.unitPrice * i.quantity; }));
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Order.prototype, "totalItems", {
+        get: function () {
+            return _.sum(_.map(this.items, function (i) { return i.quantity; }));
         },
         enumerable: false,
         configurable: true
--- ClientApp/src/app/shared/order.ts
+++ ClientApp/src/app/shared/order.ts
@@ -1,5 +1,6 @@
 import * as _ from "lodash";
-
+import { forEach } from "lodash";
+//NOTE: Put the count here make a property for this class
 export class Order {
   orderId: number;
   orderDate: Date;
@@ -8,14 +9,17 @@ export class Order {
   get subtotal(): number {
     return _.sum(_.map(this.items, i => i.unitPrice * i.quantity ));
   }
+  get totalItems(): number {
+    return _.sum(_.map(this.items, i => i.quantity)); 
+  }
 }
 export class OrderItem {
   id: number;
   quantity: number;
   unitPrice: number;
   productId: number;
-  productCategory: string;
   productName: string;
+  productCategory: string;
 }
 
 
